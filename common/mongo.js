495fd9fece721b9f9008c4a247692c32405cb04b
洛晔 <178974662@qq.com>
2017-10-09 21:09:52 +0800

    update

--- common/mongo.js
+++ common/mongo.js
@@ -1,7 +1,7 @@
 var Mongolass = require('mongolass');
 var mongolass = new Mongolass();
 let logger = require('log4js').getLogger('MongoDB');
-mongolass.connect('mongodb://localhost/splider').then(() => {
+mongolass.connect('mongodb://localhost/wechat_splider').then(() => {
 	logger.info('MongoDB connect success!');
 }, () => {
 	logger.error('MongoDB connect failed!');
--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	debug: true,
 	mongoDB: 'mongodb://localhost/wechat_splider',
-	crawlerUrl: 'https://book.douban.com/tag/'
+	crawlerUrl: 'http://weixin.sogou.com/weixin?type=2&s_from=input&ie=utf8&_sug_=y&query='
 };
